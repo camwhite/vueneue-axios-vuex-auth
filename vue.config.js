72afcc7e521a90d9878a412b7827f7e39597fa94
camwhite <codeandcam@gmail.com>
2018-08-20 18:31:34 -0600

    :nut_and_bolt: runtime cahing pwa config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,19 @@
+const proxy = require('koa-proxy')
+
 module.exports = {
+  pluginOptions: {
+    generate: {
+      scanRouter: true,
+      params: {},
+      paths: []
+    }
+  },
   pwa: {
     workboxOptions: {
+      runtimeCaching: [{
+        urlPattern: new RegExp('/api'),
+        handler: 'networkFirst'
+      }],
       templatedUrls: {
         '/': 'index.ssr.html'
       }
